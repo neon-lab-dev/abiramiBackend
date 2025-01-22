52d41a50be2015131b1d12548305cb553e883ec7
neon-lab-dev <162788012+neon-lab-dev@users.noreply.github.com>
2025-01-22 20:09:31 +0530

    Update clientRouter.js

--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createClient, deleteClient, getAllClients, updateClient , searchClients } from "../controllers/clientController.js";
+import { createClient, deleteClient, getAllClients, updateClient , searchClients, getSingleClient } from "../controllers/clientController.js";
 import { verifyTokenAdmin } from "../middlewares/requireAuth.js";
 
 const clientRouter = Router();
@@ -8,6 +8,7 @@ clientRouter.use(verifyTokenAdmin);
 clientRouter.get("/", getAllClients);
 clientRouter.post("/", createClient);
 clientRouter.put("/:id", updateClient);
+clientRouter.get("/:id", getSingleClient);
 clientRouter.delete("/:id", deleteClient);
 clientRouter.get("/search", searchClients);
 
