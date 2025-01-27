74cb6f7b7d681708939ffaa958b07ac567c5927c
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2025-01-27 09:58:07 +0545

    updates billingProduct schema

---
+++ Prisma/schema/migrations/20250127041151_update7/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `BillingDetails` MODIFY `poNO` VARCHAR(191) NULL,
+    MODIFY `vehicleNo` VARCHAR(191) NULL;
---
+++ Prisma/schema/migrations/20250127041301_update8/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `ProductDetails` MODIFY `HSNno` VARCHAR(191) NOT NULL;
